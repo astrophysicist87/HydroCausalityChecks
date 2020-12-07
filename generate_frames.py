d8889eb263f5c77d61e628827d5c1442ddae9d7c
Christopher Plumberg <plumberg.1@osu.edu>
2020-12-07 10:42:03 -0600

    Update generate_frames.py

--- generate_frames.py
+++ generate_frames.py
@@ -84,8 +84,78 @@ def generate_frame(frameNumber):
     plt.close(fig)
 
 #===============================================================================
+#def identify_violations(piViolations, BulkPiViolations):
+#    # convert pi to make it easier to process
+#    piViolation[:,1] = int(np.round(1000.0*piViolation[:,1]))
+    
+
+#===============================================================================
+def generate_frame_wRegulation(frameNumber):
+    # load data to plot
+    global tau
+    frameData = np.loadtxt(inpath + '/frame%(frame)03d.dat' % {'frame': frameNumber})
+    if frameData.size != 0:
+        tau = frameData[0,2]
+        frameData = np.unique(frameData, axis=0)
+        if energyCutOff:
+            frameData = frameData[np.where(frameData[:,6] >= eDec)]
+            
+    if frameData.size == 0:
+        frameData = np.array([[0,0,0,0,0,0,0,0], [0,0,0,0,0,0,0,0]])
+        
+    dataToPlot = frameData[:,[4,3]]     # swap x and y to get correct orientation
+
+    fig, axs = plt.subplots( nrows=1, ncols=2 )
+    
+    # histogram with each entry weighted by causality conditions
+    vals = np.array([colorFunction(entry) for entry in frameData])
+    H, xedges, yedges = np.histogram2d(dataToPlot[:,0], dataToPlot[:,1], \
+                        bins=(nxbins, nybins), weights=vals, \
+                        range=[[-scalex-0.5*dx,scalex+0.5*dx],
+                               [-scaley-0.5*dy,scaley+0.5*dy]])
+                
+    axs[0,0].imshow(H.astype(int), interpolation='nearest', origin='low', \
+                  extent=[-scalex-0.5*dx,scalex+0.5*dx,-scaley-0.5*dy,scaley+0.5*dy], \
+                  cmap=ListedColormap(['black','red','purple','blue']),
+                  vmin=0, vmax=3)
+    
+    # load piViolation and BulkPiViolation files to track where regulator is needed
+    # (assumed to be one directory level up)
+    piViolations = np.loadtxt(inpath + '/../piViolation.dat')
+    BulkPiViolations = np.loadtxt(inpath + '/../BulkpiViolation.dat')
+    piViolations = piViolations[np.where( np.isclose(piViolations[:,0], tau) & (piViolations[:,1]>0) )]
+    BulkPiViolations = BulkPiViolations[np.where( np.isclose(BulkPiViolations[:,0], tau) )]
+
+    # swap x and y for consistency
+    dataToPlot = np.vstack( (piViolations[:,[-2,-1]], BulkPiViolations[:,[-2,-1]]) )
+
+    H, xedges, yedges = np.histogram2d(dataToPlot[:,0], dataToPlot[:,1], \
+                                    bins=(nxbins, nybins), \
+                                    range=[[-scalex-0.5*dx,scalex+0.5*dx],
+                                           [-scaley-0.5*dy,scaley+0.5*dy]])
+        
+    axs[0,1].imshow(H.astype(int), interpolation='nearest', origin='low', \
+                  extent=[-scalex-0.5*dx,scalex+0.5*dx,-scaley-0.5*dy,scaley+0.5*dy], \
+                  cmap=ListedColormap(['black','white']), vmin=0, vmax=1)
+                  
+    plt.text(0.075, 0.925, r'$\tau = %(t)5.2f$ fm$/c$'%{'t': tau}, \
+            {'color': 'white', 'fontsize': 12}, transform=axs[0,0].transAxes,
+            horizontalalignment='left', verticalalignment='top')
+            
+    for ax in axs.ravel():
+        ax.set_xlabel(r'$x$ (fm)', fontsize=16)
+        ax.set_ylabel(r'$y$ (fm)', fontsize=16)
+    
+    #plt.show()
+    outfilename = outpath + '/slide_wReg%(frame)03d.png' % {'frame': frameNumber}
+    print 'Saving to', outfilename
+    fig.savefig(outfilename, bbox_inches='tight')
+    plt.close(fig)
+
+#===============================================================================
 if __name__ == "__main__":
     # generate frames one by one
     for frameNumber in range(minFrameNumber, maxFrameNumber):
         print 'Generating frame =', frameNumber
         generate_frame(frameNumber)
+        generate_frame_wRegulation(frameNumber)
