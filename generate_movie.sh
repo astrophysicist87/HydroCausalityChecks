78343691c02ad1f734d22847be026f6d6940641b
Plumberg <plumberg@cc-login1.campuscluster.illinois.edu>
2021-03-10 13:13:25 -0600

    Merged local updates.

--- generate_movie.sh
+++ generate_movie.sh
@@ -7,17 +7,19 @@ filePath=`readlink -e $1`
 fileDirec=`dirname $filePath`
 fileName=`basename $filePath`
 
+eDec=$4
+
 (
 	cd $fileDirec
-	rm -rf frames slides out.mp4
+	rm -rf frames slides out.mp4 out_wReg.mp4
 	mkdir frames
 	mkdir slides
 
 	# assuming same number of lines for each tau...
-	split --lines=$3 -d --suffix-length=3 $fileName frames/frame
+	split --lines=$3 -d --suffix-length=4 $fileName frames/frame
 	nFiles=`\ls -1 frames/frame* | wc -l`
 	#for ((i=$nFiles; i<=$[nFiles+10]; i++))
-	for i in $(seq -f "%03g" $nFiles $[nFiles+10])
+	for i in $(seq -f "%04g" $nFiles $[nFiles+10])
 	do
 		echo > frames/frame${i}
 	done
@@ -28,17 +30,17 @@ fileName=`basename $filePath`
 )
 	i=`\ls -1 $fileDirec/frames/frame* | wc -l`
 	# This generates frames for the animations
-	echo 'Executing python generate_frames.py '$2' 0 '$i' '$fileDirec'/frames '$fileDirec'/slides'
-	python generate_frames.py $2 0 $i $fileDirec/frames $fileDirec/slides
+	echo 'Executing python generate_frames.py '$2' 0 '$i' '$fileDirec'/frames '$fileDirec'/slides' $4 $5
+	python generate_frames.py $2 0 $i $fileDirec/frames $fileDirec/slides $4 $5
 
-	# This generates frame sequence plots specifically for paper
-	echo 'Executing python generate_plots.py '$2' 0 '$i' '$fileDirec'/frames '$fileDirec'/slides'
-	python generate_plots.py $2 0 $i $fileDirec/frames $fileDirec/slides
+        # This generates frame sequence plots specifically for paper
+        echo 'Executing python generate_plots.py '$2' 0 '$i' '$fileDirec'/frames '$fileDirec'/slides' $4 $5
+        python generate_plots.py $2 0 $i $fileDirec/frames $fileDirec/slides $4 $5
 
 (
 	cd $fileDirec
 	framesPerSecond=60
-	pngs2mp4 $framesPerSecond slides/slide%03d.png out.mp4  
-    framesPerSecond=5
-    pngs2mp4 $framesPerSecond slides/slide_wReg%03d.png out_wReg.mp4
+	pngs2mp4 $framesPerSecond slides/slide%04d.png out.mp4  
+        #framesPerSecond=5
+        #pngs2mp4 $framesPerSecond slides/slide_wReg%04d.png out_wReg.mp4
 )
