951e5970d8df42fa7c56cba71281a76cab05afc6
Christopher Plumberg <plumberg.1@osu.edu>
2021-03-10 20:47:37 -0600

    Update run_all_jobs.sh

--- run_all_jobs.sh
+++ run_all_jobs.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 #----------
 
-for file in ../OSU_hydro/RESULTS_OPTIMAL_FINAL_NOFS/viscous_14_moments_evo.dat
-do
-	path=`readlink -e $file`
-	sbatch --export=ALL,file_to_check=`echo $path`,inGridScale=12.189,inGridSize=$[171**2],inEDecoupling=0.26511743083794326,inDXY=0.1434 run_all.sbatch
-done
-
-
-#for file in ../MUSIC/results_t0_*fm_wBulk/evolution_full.dat ../MUSIC/results_noKompost_wBulk/evolution_full.dat
+#for file in ../OSU_hydro/RESULTS_OPTIMAL_FINAL_NOFS/viscous_14_moments_evo.dat
 #do
-#        path=`readlink -e $file`
-#        sbatch --export=ALL,file_to_check=`echo $path`,inGridScale=20.48,inGridSize=$[512**2],inEDecoupling=0.3,inDXY=0.08 run_all.sbatch
+#	path=`readlink -e $file`
+#	sbatch --export=ALL,file_to_check=`echo $path`,inGridScale=12.189,inGridSize=$[171**2],inEDecoupling=0.26511743083794326,inDXY=0.1434 run_all.sbatch
 #done
+
+
+for file in ../MUSIC/results_PbPb*Kompost/evolution_full.dat
+do
+        path=`readlink -e $file`
+        sbatch --export=ALL,file_to_check=`echo $path`,inGridScale=20.48,inGridSize=$[512**2],inEDecoupling=0.3,inDXY=0.08 run_all.sbatch
+done
 
