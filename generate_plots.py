9e46a6d08acd78cedcc1e74736d807ce6822bbd2
Christopher Plumberg <plumberg.1@osu.edu>
2021-03-13 16:33:21 -0600

    Update generate_plots.py

--- generate_plots.py
+++ generate_plots.py
@@ -90,7 +90,9 @@ def generate_frames(frameNumbers):
         print('xedges.shape =', xedges.shape)
         print('yedges.shape =', yedges.shape)
         print('H.shape =', H.shape)
-        H = (H[ np.where( np.abs(xedges)<=17.0 ), np.where( np.abs(yedges)<=17.0 ) ]).T
+        H = H[ np.where( np.abs(xedges)<=17.0 ) ]
+        H = H[ :, np.where( np.abs(yedges)<=17.0 ) ]
+        H = H.T
         print('H.shape =', H.shape)
         print(1/0)
         axs[i].imshow(H.astype(int), interpolation='nearest', origin='low', \
