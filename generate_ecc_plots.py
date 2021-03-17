bba465ef2e40bff27f60a262b6407b717a0075e3
Christopher Plumberg <plumberg.1@osu.edu>
2021-03-17 06:54:07 -0500

    Update generate_ecc_plots.py

--- generate_ecc_plots.py
+++ generate_ecc_plots.py
@@ -18,7 +18,7 @@ tau = 0.6   #initial tau (fm/c), overwritten by value in file
 
 energyCutOff = False
 eDec = 0.3/hbarc  # impose cut off in fm^{-4}
-eDec = float(sys.argv[4])/hbarc
+#eDec = float(sys.argv[4])/hbarc
 
 colorsToUse = ['black','red','purple','blue','green','orange']
 
