c1bbb7b81d387fb81f46184c91fdf40c10030e1e
Christopher Plumberg <plumberg.1@osu.edu>
2021-04-07 10:45:12 -0500

    Update plot_KR_frame.py

--- plot_KR_frame.py
+++ plot_KR_frame.py
@@ -4,6 +4,8 @@ matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import os, sys
 
+hbarc = 0.19733
+
 #path='C:/Users/Christopher Plumberg/Desktop/Research/UIUC/HydroCausalityChecks/KRevo_frame0000.dat'
 inpath=sys.argv[1]
 outpath=sys.argv[2]
@@ -13,13 +15,17 @@ KRdata = np.loadtxt(inpath)
 
 tau = KRdata[0,0]
 
+eDec=0.18
+eBelowFO = np.where(KRdata[:,3] < eDec/hbarc)
+KRdata[eBelowFO,[-2,-1]] = 0.0
+
 fig, ax = plt.subplots( nrows=1, ncols=1, figsize=(6,6) )
 im = ax.imshow(KRdata[:,-2].reshape([512,512]), interpolation='nearest', origin='lower', \
                       extent=[-20.48,20.48,-20.48,20.48], \
                       cmap=plt.get_cmap('gnuplot2'), vmin=0.0, vmax=5.0)
 
 cbar = plt.colorbar(im, fraction=0.046, pad=0.04)
-cbar.set_label(label=r'$R_\pi$', size=16, weight='bold')
+cbar.set_label(label=r'$R^{-1}_\pi$', size=16, weight='bold')
 
 plt.text(0.075, 0.925, r'$\tau = %(t)5.2f$ fm$/c$'%{'t': tau}, \
         {'color': 'white', 'fontsize': 12}, transform=ax.transAxes,
