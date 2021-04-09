dc5bde7bde1599195b5acb228f76e22ab896c22f
Christopher Plumberg <plumberg.1@osu.edu>
2021-04-09 07:52:58 -0500

    Update plot_KR_frame.py

--- plot_KR_frame.py
+++ plot_KR_frame.py
@@ -10,8 +10,13 @@ hbarc = 0.19733
 inpath=sys.argv[1]
 outpath=sys.argv[2]
 #nFiles=int(sys.argv[3])
+#gridSize = 512 if len(sys.argv) < 4 else int(sys.argv[3])
+#gridScale = 20.48 if len(sys.argv) < 5 else int(sys.argv[4])
 
 KRdata = np.loadtxt(inpath)
+
+gridSize = int(np.sqrt(len(KRdata[:,1])))
+gridScale = -KRdata[0,1]
 
 tau = KRdata[0,0]
 
@@ -25,8 +30,8 @@ RPi[eBelowFO] = 0.0
 #===========================================================
 # Plot pi
 fig, ax = plt.subplots( nrows=1, ncols=1, figsize=(6,6) )
-im = ax.imshow(Rpi.reshape([512,512]), interpolation='nearest', origin='lower', \
-                      extent=[-20.48,20.48,-20.48,20.48], \
+im = ax.imshow(Rpi.reshape([gridSize,gridSize]), interpolation='nearest', origin='lower', \
+                      extent=[-gridScale,gridScale,-gridScale,gridScale], \
                       cmap=plt.get_cmap('gnuplot2'), vmin=0.0, vmax=1.0)
 
 cbar = plt.colorbar(im, fraction=0.046, pad=0.04)
@@ -50,8 +55,8 @@ plt.close(fig)
 #===========================================================
 # Reset, plot Pi
 fig, ax = plt.subplots( nrows=1, ncols=1, figsize=(6,6) )
-im = ax.imshow(RPi.reshape([512,512]), interpolation='nearest', origin='lower', \
-                      extent=[-20.48,20.48,-20.48,20.48], \
+im = ax.imshow(RPi.reshape([gridSize,gridSize]), interpolation='nearest', origin='lower', \
+                      extent=[-gridScale,gridScale,-gridScale,gridScale], \
                       cmap=plt.get_cmap('gnuplot2'), vmin=0.0, vmax=1.0)
 
 cbar = plt.colorbar(im, fraction=0.046, pad=0.04)
