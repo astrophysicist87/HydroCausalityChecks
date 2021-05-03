bb5d928c7183a925ec7561360217dc5e0a66b7d2
Christopher Plumberg <plumberg.1@osu.edu>
2021-05-03 12:35:16 -0400

    Update generate_pPb_plots.py

--- generate_pPb_plots.py
+++ generate_pPb_plots.py
@@ -110,15 +110,15 @@ def generate_frames(frameNumbers):
         
 
 
-    #plt.text(0.075, 0.15, hydroString, \
+    plt.text(0.075, 0.15, hydroString, \
+        {'color': 'white', 'fontsize': 12}, transform=axs.transAxes,
+        horizontalalignment='left', verticalalignment='top')
+    #plt.text(0.075, 0.175, 'T$_{\\rm R}$ENTo + free-streaming', \
     #    {'color': 'white', 'fontsize': 12}, transform=axs.transAxes,
     #    horizontalalignment='left', verticalalignment='top')
-    plt.text(0.075, 0.175, 'T$_{\\rm R}$ENTo + free-streaming', \
-        {'color': 'white', 'fontsize': 12}, transform=axs.transAxes,
-        horizontalalignment='left', verticalalignment='top')
-    plt.text(0.25, 0.1, '+ iEBE-VISHNU', \
-        {'color': 'white', 'fontsize': 12}, transform=axs.transAxes,
-        horizontalalignment='left', verticalalignment='top')
+    #plt.text(0.25, 0.1, '+ iEBE-VISHNU', \
+    #    {'color': 'white', 'fontsize': 12}, transform=axs.transAxes,
+    #    horizontalalignment='left', verticalalignment='top')
     
     plt.text(0.75, 0.925, 'p+Pb', \
         {'color': 'white', 'fontsize': 14}, transform=axs.transAxes,
